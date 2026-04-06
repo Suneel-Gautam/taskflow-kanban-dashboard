732153229e917e537b18001cb543027e051fd468
Sunil Gautam <gautamsunil917@gmail.com>
2026-04-06 16:21:48 +0545

    delete , save in localStorage Completed

--- script.js
+++ script.js
@@ -1,106 +1,138 @@
-const toDo = document.querySelector('.to-do')
-const progress = document.querySelector('.progress')
-const done = document.querySelector('.done')
-const addTaskOpenModal = document.querySelector('#addTask')
+const toDo = document.querySelector(".to-do");
+const progress = document.querySelector(".progress");
+const done = document.querySelector(".done");
+const addTaskOpenModal = document.querySelector("#addTask");
+
+
+const todoContainer = document.querySelector('.todoTaskContainer')
+const progressContainer = document.querySelector('.progressTaskContainer')
+const doneContainer = document.querySelector('.donetaskContainer')
+
+
+console.log(localStorage.getItem('taskarray'))
+
+function handleSaveLocalStorage(obj) {
+
+    if (localStorage.getItem('taskarray') === null) {
+        let taskarry = []
+        taskarry.push(obj)
+        localStorage.setItem('taskarray', JSON.stringify(taskarry))
+    } else {
+        let taskarry = JSON.parse(localStorage.getItem('taskarray'))
+        taskarry.push(obj)
+        localStorage.setItem('taskarray', JSON.stringify(taskarry))
+    }
+}
 
 let dropElement;
 
-function HandleDragElement(column) {
-    column.addEventListener('dragover', (e) => {
-        e.preventDefault()
-    })
-    column.addEventListener('dragenter', (e) => {
-        e.preventDefault()
-        column.classList.add('onEnter')
-    })
-    column.addEventListener('dragleave', (e) => {
-        e.preventDefault()
-        column.classList.remove('onEnter')
-    })
-    column.addEventListener('drop', (e) => {
-        e.preventDefault()
-        column.append(dropElement)
-        column.classList.remove('onEnter')
-    })
+function HandleDragElement(column, container) {
+    column.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    });
+    column.addEventListener("dragenter", (e) => {
+        e.preventDefault();
+        column.classList.add("onEnter");
+    });
+    column.addEventListener("dragleave", (e) => {
+        e.preventDefault();
+        column.classList.remove("onEnter");
+    });
+    column.addEventListener("drop", (e) => {
+        e.preventDefault();
+        container.append(dropElement);
+        column.classList.remove("onEnter");
+    });
 }
 
-HandleDragElement(toDo)
-HandleDragElement(progress)
-HandleDragElement(done)
+HandleDragElement(toDo, todoContainer);
+HandleDragElement(progress, progressContainer);
+HandleDragElement(done, doneContainer);
 
+const addContainer = document.querySelector("#addcontainer");
+const addTaskButton = document.querySelector("#AddTaskButton");
+const closeTaskContainer = document.querySelector("#closeButton");
 
+addTaskOpenModal.addEventListener("click", () => {
+    addContainer.style.display = "flex";
+});
 
-const addContainer = document.querySelector('#addcontainer')
-const addTaskButton = document.querySelector('#AddTaskButton')
-const closeTaskContainer = document.querySelector('#closeButton')
+closeTaskContainer.addEventListener("click", () => {
+    addContainer.style.display = "none";
+});
 
-addTaskOpenModal.addEventListener('click', () => {
-    addContainer.style.display = 'flex'
-})
+const taskName = document.querySelector("#taskName");
+const taskDescription = document.querySelector("#taskDescription");
+const errorMessage = document.querySelector(".error");
 
-closeTaskContainer.addEventListener('click', () => {
-    addContainer.style.display = 'none'
-})
-
-const taskName = document.querySelector('#taskName')
-const taskDescription = document.querySelector('#taskDescription')
-const errorMessage = document.querySelector('.error')
-
-taskName.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        taskDescription.focus()
+taskName.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        taskDescription.focus();
     }
+});
+taskName.addEventListener("input", () => {
+    errorMessage.innerHTML = "";
+});
 
-})
-
-addTaskButton.addEventListener('click', () => {
-    let taskNameValue = taskName.value.trim()
-    let taskDescriptionValue = taskDescription.value.trim()
+addTaskButton.addEventListener("click", () => {
+    let taskNameValue = taskName.value.trim();
+    let taskDescriptionValue = taskDescription.value.trim();
 
     if (taskNameValue === "") {
-        errorMessage.innerHTML = 'Please Enter Task '
-        return
+        errorMessage.innerHTML = "Please Enter Task ";
+        return;
     }
-    displayTaskCard({
+    handleSaveLocalStorage({
         taskNameValue,
-        taskDescriptionValue
-    })
-    taskName.value = ''
-    taskDescription.value = ''
-    addContainer.style.display = 'none'
-
-})
-
-function displayTaskCard(obj) {
-    const div = document.createElement('div')
-    div.draggable = true
-    div.classList.add('task')
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `${obj.taskNameValue}`
-    div.append(h2)
-    const p = document.createElement('p')
-    p.innerHTML = `${obj.taskDescriptionValue}`
-    div.append(p)
-    const buttonDiv = document.createElement('div')
-    div.append(buttonDiv)
-    const button = document.createElement('button')
-    button.innerHTML = 'delete'
-    buttonDiv.append(button)
-    toDo.append(div)
+        taskDescriptionValue,
+        taskStatus: "Todo",
+    });
+    displayTaskCard()
+    taskName.value = "";
+    taskDescription.value = "";
+    addContainer.style.display = "none";
+});
 
 
-    const tasks = document.querySelectorAll('.task')
-    tasks.forEach(task => {
-        task.addEventListener('drag', (e) => {
-            dropElement = task
+
+function displayTaskCard() {
+
+    todoContainer.innerHTML = ""
+    let taskarray = JSON.parse(localStorage.getItem('taskarray'))
+    taskarray.forEach((Element, index) => {
+        const div = document.createElement("div");
+        div.draggable = true;
+        div.classList.add("task");
+        const h2 = document.createElement("h2");
+        h2.innerHTML = `${Element.taskNameValue}`;
+        div.append(h2);
+        const p = document.createElement("p");
+        p.innerHTML = `${Element.taskDescriptionValue}`;
+        div.append(p);
+        const buttonDiv = document.createElement("div");
+        div.append(buttonDiv);
+        const button = document.createElement("button");
+        button.innerHTML = "delete";
+        buttonDiv.append(button);
+        todoContainer.append(div);
+
+
+        button.addEventListener('click', () => {
+            let array = JSON.parse(localStorage.getItem('taskarray'))
+            array.splice(index, 1)
+            localStorage.setItem('taskarray', JSON.stringify(array))
+            displayTaskCard()
         })
     })
 
-
-
+    const tasks = document.querySelectorAll(".task");
+    tasks.forEach((task) => {
+        task.addEventListener("drag", (e) => {
+            dropElement = task;
+        });
+    });
 }
 
-displayTaskCard()
 
 
-
+displayTaskCard();
